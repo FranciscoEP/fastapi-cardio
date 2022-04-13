a0f8115698ea9dff6e8e905997f8b1c133b512bb
FranciscoEP <francisco.eugp@gmail.com>
2022-04-12 21:49:16 -0500

    request_response_body

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
 from fastapi import FastAPI
+from fastapi import Body
 
 app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "Hello World"}
+    return {"message": "Status OK"}
+
+# Models
+class User(BaseModel):
+    first_name: str
+    last_name: str
+    email: str
+    password: str
+    photo_url: Optional[str] = None
+    is_active: Optional[bool] = True
+
+
+# Request and Response body
+@app.post('/user/new')
+def create_user(user: User = Body(...)):
+    return user
     
