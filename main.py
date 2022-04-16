966bc7fee27c7cabe4a32b8529e1441dfe3ab8f1
FranciscoEP <francisco.eugp@gmail.com>
2022-04-15 19:52:42 -0500

    Documentarion app added

--- main.py
+++ main.py
@@ -76,7 +76,11 @@ class Login_Out(BaseModel):
 # Request and Response body
 app = FastAPI()
 
-@app.get(path="/", status_code=status.HTTP_200_OK)
+@app.get(
+    path="/", 
+    status_code=status.HTTP_200_OK,
+    tags=['Home']
+)
 def home():
     return {"message": "Status OK"}
 
@@ -84,9 +88,24 @@ def home():
 @app.post(
     path='/user/new',
     response_model=User_Out,
-    status_code=status.HTTP_201_CREATED
-    )
+    status_code=status.HTTP_201_CREATED,
+    tags=['Users'],
+    summary="Create user in the app"
+)
 def create_user(user: User_In = Body(...)):
+    """
+    Create a new user
+
+    This path operation creates a user in the app and store the information in the database.
+
+    Parameters:
+    - Request body parameter:
+        - **user: User** -> User model with first_name, last_name, email, password, credit_card_number, role, photo_url, is_active
+
+    Returns a user created with first_name, last_name, email, credit_card_number, role, photo_url, is_active attributes.
+    
+    
+    """
     return user
     
 # Simple Validations
@@ -94,7 +113,8 @@ users = [1, 2, 3, 4, 5]
 
 @app.get(
     path='/user/detail', 
-    status_code=status.HTTP_200_OK
+    status_code=status.HTTP_200_OK,
+    tags=['Users']
     )
 def get_user_detail(
     age: Optional[str] = Query(
@@ -112,9 +132,24 @@ def get_user_detail(
     
 
 ):
+    """
+    Show user's information
+
+    This path operation display the user's information the app.
+
+    Parameters:
+    - Request body parameter:
+        - **age: Str** -> Attribute that returns user's Age. String type.
+        - **description: Str** -> Attribute that returns user's description. String type.
+
+    Returns user's description and age.
+    """
     return {"age": age, "description": description}
 
-@app.get('/user/detail/{user_id}', status_code=status.HTTP_200_OK)
+@app.get(
+    path='/user/detail/{user_id}', 
+    status_code=status.HTTP_200_OK,
+    tags=['Users'])
 def get_user_detail(
     user_id: int = Path(..., 
         gt=0,
@@ -122,37 +157,82 @@ def get_user_detail(
         description="User's unique identifier",
     )
 ):
+    """
+    Show user's information
+
+    This path operation display the user's information in the app.
+
+    Parameters:
+    - Request body parameter:
+        - **user_id: Int** Mandatory-> Attribute that identifies the user by Id. Int type. Primary key.
+
+    Returns user's id.
+    """
     if user_id not in users:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
     return {user_id: "Exists"}
 
 
 #Validations: Request body
-@app.put("/user/update/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
+@app.put(
+    path="/user/update/{user_id}", 
+    status_code=status.HTTP_204_NO_CONTENT,
+    tags=['Users'])
 def update_user(
     user_id: int = Path(..., 
         gt=0,
         title="User ID",
         description="User's unique identifier",
+        
     ),
     user: User_In = Body(...),
     # location: Location = Body(...)
 ):  
     # results = user.dict()
     # results.update(location.dict())
+    """
+    Update user's information
 
+    This path operation updates the user's information in the app.
+
+    Parameters:
+    - Request body parameter:
+        - **user_id: Int** Mandatory-> Attribute that identifies the user by Id. Int type. Primary key.
+        - **user: User** Mandatory-> Attributes that are going to be updated. User model with first_name, last_name, email, password, credit_card_number, role, photo_url, is_active
+
+    Returns User Information updated.
+    """
     return user
 
-@app.post(path="/user/login", response_model=Login_Out, status_code=status.HTTP_200_OK)
+@app.post(
+    path="/user/login",
+    response_model=Login_Out,
+    status_code=status.HTTP_200_OK,
+    tags=['Users']
+    )
 def login(
     username: str = Form(...),
     password: str = Form(...),
-):
+):  
+    """
+    Log in the user in App
+
+    This path operation log in the user in the app.
+
+    Parameters:
+    - Request body parameter:
+        - **username: Str** Mandatory-> Attribute that identifies the user by username. String type.
+        - **password: User** Mandatory-> Attribute that let user get in the app. String type.
+
+    Returns username.
+    """
     return Login_Out(username=username)
 
 # Cookies and headers parameters
 @app.post(
-    path="/contact", status_code=status.HTTP_200_OK)
+    path="/contact", 
+    status_code=status.HTTP_200_OK,
+    tags=['Contact'])
 def contact(
     first_name: str = Form(
     ...,
@@ -175,15 +255,43 @@ def contact(
     ads: Optional[str]= Cookie(default=None)
 
 ):
+    """
+    Input a possible user information from a Contact Form.
+
+    This path operation obtain possible user information from a Contact Form.
+
+    Parameters:
+    - Request body parameter:
+        - **first_name: Str** Mandatory-> Attribute that identifies the user by its first name. String type.
+        - **last_name: Str** Mandatory->  Attribute that identifies the user by its last name. String type.
+        - **email: Str** Mandatory-> Attribute that identifies the user by its email. String type.
+        - **user_agent: Header** Mandatory-> Attribute that displays the user_agent generated by user. Header type.
+        - **ads: Cookie** Mandatory-> Attribute that displays cookies information. Cookie type.
+
+    Returns the user_agent.
+    """
     return user_agent
 
 
 @app.post(
-    path="/post-image"
+    path="/post-image",
+    status_code=status.HTTP_200_OK,
+    tags=['Images']
 )
 def post_image(
     image: UploadFile = File(...)
 ):
+    """
+    Receives an image file.
+
+    This path operation obtain an image file and returns the name, the format and its size.
+
+    Parameters:
+    - Request body parameter:
+        - **image: Upload File** Mandatory-> Attribute thet receives an image file. File type.
+
+    Returns the image name, the format and its size.
+    """
     return {"Filename": image.filename,
     "Format": image.content_type,
     "Size(kb)": round(len(image.file.read()) / 1024, 2)
